f6454ec6182275d85ca9865814a65b70a3084678
Jim Huang <jserv@ccns.ncku.edu.tw>
2026-09-20 13:09:33 +0800

    Harden wiki sync

--- sync.sh
+++ sync.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/usr/bin/env bash
 # Mirror pages from the CSIE wiki data repo into Jekyll pages.
-set -eu
+set -euo pipefail
 
 RAW=https://raw.githubusercontent.com/NCKUCSIE-Wiki/csiewiki-wikidata/refs/heads/main
 WIKI=https://wiki.csie.ncku.edu.tw
@@ -8,9 +8,13 @@ INTRO='https://docs.google.com/presentation/d/1RwBZQDAgg0DRADH1lk06y4LEMcB1d_uYn
 
 # mirror <wiki path> <output file> <permalink>
 mirror() {
+    local tmp
+    tmp=$(mktemp "$2.XXXXXX")
+    trap 'rm -f "$tmp"' EXIT
+
     curl -fsSL "$RAW/$1.page" |
     # The wiki closes its front matter with "..."; Jekyll insists on "---".
-    awk -v wiki="/$1" -v link="$3" '
+    awk -v source="$1.page" -v wiki="/$1" -v link="$3" '
         NR > 1 && !done && /^\.\.\.$/ {
             print "layout: default"
             print "permalink: " link
@@ -19,14 +23,24 @@ mirror() {
             done = 1
             next
         }
-        { print }' |
+        { print }
+        END {
+            if (!done) {
+                print source ": missing YAML front-matter terminator" > "/dev/stderr"
+                exit 1
+            }
+        }' |
     sed \
         -e "s#\(\[Course Introduction\](\)[^)]*#\1$INTRO#" \
         -e 's#](/User/jserv)#]({{ site.baseurl }}/User/jserv)#g' \
         -e 's#](/arch/schedule)#]({{ site.baseurl }}/)#g' \
         -e "s#\](/#]($WIKI/#g" \
         -e 's#\$\\to\$#\&rarr;#g' \
-        > "$2"
+        > "$tmp"
+
+    chmod 644 "$tmp"
+    mv "$tmp" "$2"
+    trap - EXIT
 }
 
 mkdir -p User
